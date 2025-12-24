c576f4fae3e439b9d3d3d827f3fcdb445c6b25a5
Augusto Altenhofen <augustoaltenhofen@protonmail.com>
2025-12-24 17:45:09 -0300

    test: test simple qrcode generation with PixQrCodeBuilder

---
+++ qrcode/src/test/java/io/github/altenhofen/openpixapi/qrcode/PixQrCodeBuilderTest.java
@@ -0,0 +1,24 @@
+package io.github.altenhofen.openpixapi.qrcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PixQrCodeBuilderTest {
+    private final String payload = "00020101021126570014BR.GOV.BCB.PIX2535HTTPS://PIX.EXAMPLE.COM/API/WEBHOOK5204000053039865802BR5908JOHN DOE6012PORTO ALEGRE621905150512TX1234567896304B7DF";
+
+    @Test
+    public void buildPng() throws IOException, PixQrGenerationException {
+        Image image = PixQrCodeBuilder.from(payload)
+                .background(Color.BLACK)
+                .foreground(Color.YELLOW)
+                .size(1000)
+                .toPng();
+
+        assertNotNull(image);
+    }
+
+}
